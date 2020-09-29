9a64de1f2ecd2922dcfffc8027b07aede022a3b0
Yuri Halushko <36885543+snaipp1@users.noreply.github.com>
2020-09-29 14:18:42 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const check = (str, bracketsConfig) => { 
+    let bracket = str;
+    let brackets = [];
+    bracketsConfig.forEach(item => brackets.push(item[0].concat(item[1])));
+
+    for(let i = 0; i < brackets.length; i++){
+        bracket = bracket.replace(brackets[i], '');
+    }
+    if(bracket === str){
+        return false;
+    }else if(bracket === ''){
+        return true;
+    }else{
+        return check(bracket, bracketsConfig);
+    }
 }
+
+  
+
+module.exports = check; 
